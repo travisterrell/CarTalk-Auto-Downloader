7b7f98dec884e214d48818b9ee30a1f9705c2c04
Travis Terrell <tterrell@digonex.com>
2024-02-21 12:17:04 -0600

    Add configurable retry setting, improve console logging & readme (for the maybe 2 people who will ever use this, haha)

--- source/index.ts
+++ source/index.ts
@@ -2,11 +2,11 @@
 
 import chalk from 'chalk';
 import figlet from 'figlet';
+import fs from 'fs';
+import he from 'he';
 import program from 'commander';
 import puppeteer from 'puppeteer';
-import fs from 'fs';
 import request from 'request';
-import he from 'he';
 import * as settings from "./settings.json";
 const domQueries = settings.queries;
 
@@ -16,7 +16,9 @@ async function doStuff(parameters: { showBrowser: boolean, outputFolder: string,
     await page.goto(settings.carTalkPage, { waitUntil: 'networkidle2' });
 
     console.log(chalk.blueBright(figlet.textSync('Car Talk Downloader', { horizontalLayout: 'full' })));
-    console.log("Start");
+
+    console.log(chalk.bold.bgMagenta(`\n Download path: '${program.outputFolder}' \n`));
+    console.log(chalk.green("Expanding page to expose all download buttons..."));
 
     await page.addScriptTag({ url: 'https://code.jquery.com/jquery-3.2.1.min.js' });
 
@@ -36,15 +38,15 @@ async function doStuff(parameters: { showBrowser: boolean, outputFolder: string,
         if (buttonHandle) {
             await buttonHandle.evaluate(b => b.scrollIntoView());
 
-            await buttonHandle.click().catch(e => console.log(`Error clicking button: ${e.message}`));
-            console.log("Trying to load more...");
+            await buttonHandle.click().catch(e => console.log(chalk.red(`Error clicking button: ${e.message}`)));
 
+            if (settings.moreLogging) { console.log(chalk.dim.white("Trying to load more...")); }
             await page.waitFor(settings.loadMoreDelay_ms);
         }
 
         currentDownloadButtonCount = (await page.$$(domQueries.downloadButtons)).length;
 
-        console.log(`Found ${currentDownloadButtonCount} download buttons so far.`);
+        console.log(chalk.dim.white(`Found ${currentDownloadButtonCount} download buttons so far.`));
 
         isNotHidden = await page.evaluate(selector => {
             const elem = document.querySelector(selector);
@@ -57,15 +59,15 @@ async function doStuff(parameters: { showBrowser: boolean, outputFolder: string,
             currentLoadMoreTryCount = 0;
         }
 
-        if (currentLoadMoreTryCount > 5) {
-            console.log(`No more buttons have been loaded after ${currentLoadMoreTryCount} attempts. Starting download process.`);
+        if (currentLoadMoreTryCount > settings.loadMoreMaxAttempts) {
+            console.log(chalk.yellowBright(`No more buttons have been loaded after ${currentLoadMoreTryCount} attempts. Starting download process.`));
             break;
         }
 
         previousDownloadButtonCount = currentDownloadButtonCount;
     }
 
-    console.log("Page fully expanded");
+    console.log(chalk.blueBright("Page fully expanded"));
 
     const rawButtonData = await page.$$eval(domQueries.downloadButtons, buttons => buttons.map(button => {
         const link = button.getAttribute("href");
@@ -84,24 +86,20 @@ async function doStuff(parameters: { showBrowser: boolean, outputFolder: string,
         const fileName = getFileName(title);
 
         if (existingFiles.has(fileName)) {
-            console.log(`File ${fileName} already exists.`);
+            console.log(chalk.yellow(`File ${fileName} already exists.`));
             continue;
         }
 
-        console.log(`Downloading '${fileName}' (${currentDownloadCount}/${buttonData.length})`);
+        console.log(chalk.blue(`Downloading '${fileName}' (${currentDownloadCount}/${buttonData.length})`));
         // Important Fix: TypeScript assertion that `link` is not null.
-        await downloadAudioFromLinkAsync(link!, fileName, folderPath).catch(error => console.log(`Error in downloading: ${error}`));
+        await downloadAudioFromLinkAsync(link!, fileName, folderPath).catch(error => console.log(chalk.red(`Error in downloading: ${error}`)));
         currentDownloadCount++;
     }
 
-    console.log(`Downloaded ${buttonData.length} files to '${parameters.outputFolder}'`);
+    console.log(chalk.green(`\n Downloaded ${buttonData.length} files to '${parameters.outputFolder}'`));
 
     await page.close();
     await browser.close(); // Ensure the browser is closed after operation
-}
-
-function notNullOrUndefined<T>(value: T | null | undefined): value is T {
-    return value !== null && value !== undefined;
 }
 
 async function downloadAudioFromLinkAsync(link: string, fileName: string, folderPath: string) {
@@ -112,11 +110,11 @@ async function downloadAudioFromLinkAsync(link: string, fileName: string, folder
         let stream = request(link)
             .pipe(filePipe)
             .on('finish', () => {
-                // console.log(`The file ${fileName} is finished downloading.`);
+                if (settings.moreLogging) { console.log(`The file ${fileName} is finished downloading.`); }
                 resolve();
             })
             .on('error', (error) => {
-                console.log(`Error downloading file ${fileName}: ${error}`);
+                console.log(chalk.red(`Error downloading file ${fileName}: ${error}`));
                 reject(error);
             });
     });
@@ -131,29 +129,24 @@ function getExistingFiles(filePath: string) {
 }
 
 function getFileName(title: string | null): string {
+    if (settings.moreLogging) { console.log(chalk.dim.white(`Original title: ${title}`)); }
     if (!title) {
-        console.log("No title");
+        console.log(chalk.red("No title! Saving as Unknown_Title.mp3"));
         return "Unknown_Title.mp3";
     }
-    console.log(`Original title: ${title}`);
-
     let decodedTitle = he.decode(title);
     decodedTitle = decodedTitle.replace(':', ' -').replace('#', '').replace(/[<>:"\/\\|?*]+/g, '_');
 
     return decodedTitle.trim() + ".mp3";
 }
 
-function sleep(ms: number) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
-
 const defaultPath = getAppDataPath() + "\\cartalkad";
 
 program
     .name("cartalkad")
-    .version('0.1')
+    .version('0.1.1')
     .description('Download publicly available Car Talk episodes via a CLI!')
-    .option('--show-browser', 'Displays the web browser instance as the downloader is running.', false)
+    .option('--show-browser', 'Displays the web browser instance as the downloader is running. Helpful if you think it\'s having problems.', false)
     .option('-f, --output-folder <path>', 'Specify the output folder for downloads.', defaultPath)
     .option('-d, --dry-run', 'Run the script without downloading files to show what would be downloaded.', false)
     .option('-e, --download-new-episodes', 'Download new episodes not already present in the output directory.', false)
@@ -172,6 +165,5 @@ if (!process.argv.slice(2).length) {
 }
 
 function getAppDataPath() {
-    // return 'S:\\Audio';
     return process.env.APPDATA || (process.platform == 'darwin' ? process.env.HOME + '/Library/Preferences' : process.env.HOME + "/.local/share");
 }
